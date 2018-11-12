cf5b4b9a7983dd37972b51a067326d7cba7f96b9
manuel <manuel.rs019@gmail.com>
2018-11-11 20:26:53 -0600

    navegacion y iconos

---
+++ app/src/main/java/com/example/zeppe/relog/ConfigFragment.java
@@ -0,0 +1,39 @@
+package com.example.zeppe.relog;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ConfigFragment extends MainFragment {
+    public View view;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_config,container,false);
+        return  view;
+
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        menu.clear();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        fragmentActions.changeTitle("Configuración");
+    }
+}
